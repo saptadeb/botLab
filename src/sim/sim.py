49883b9eee75c5a9128ba90b101d838841dda074
Kevin French <fnivek@gmail.com>
2020-03-14 21:46:51 -0400

    TIMING: Make a new helper class to run code at a particular rate

--- src/sim/sim.py
+++ src/sim/sim.py
@@ -1,8 +1,10 @@
 import pygame
 from map import Map
 from mbot import Mbot
+from timing import Rate
 import math
 from pygame.locals import *
+
 
 class Gui:
     def __init__(self):
@@ -13,6 +15,7 @@ class Gui:
         self._sprites = pygame.sprite.RenderUpdates()
         self._mbot = Mbot()
         self._sprites.add(self._mbot)
+        self._frame_rate = 50
 
     def on_init(self):
         # Pygame
@@ -32,9 +35,9 @@ class Gui:
             self._running = False
 
     def on_loop(self):
-        self._mbot._pose.theta += 0.001
-        self._mbot._pose.x += 0.1 * math.cos(-self._mbot._pose.theta)
-        self._mbot._pose.y += 0.1 * math.sin(-self._mbot._pose.theta)
+        self._mbot._pose.theta += 0.1
+        self._mbot._pose.x += 1 * math.cos(-self._mbot._pose.theta)
+        self._mbot._pose.y += 1 * math.sin(-self._mbot._pose.theta)
 
     def on_render(self):
         self._sprites.clear(self._display_surf, self._map.image)
@@ -50,10 +53,12 @@ class Gui:
             self._running = False
 
         while( self._running ):
-            for event in pygame.event.get():
-                self.on_event(event)
-            self.on_loop()
-            self.on_render()
+            with Rate(self._frame_rate):
+                for event in pygame.event.get():
+                    self.on_event(event)
+                self.on_loop()
+                self.on_render()
+
         self.on_cleanup()
 
 if __name__ == "__main__" :
---
+++ src/sim/timing.py
@@ -0,0 +1,56 @@
+import time
+
+
+class Rate:
+    """!
+    @brief      Runs code inside a with block sleeping to maintain the rate
+    """
+
+    def __init__(self, rate=None, period=None):
+        """!
+        @brief      Constructs a new instance.
+
+        @param      rate    The rate in Hz
+        @param      period  The period is seconds
+
+        Exactly one of rate or period may be used if both or none are specified an error will be raised
+        """
+        if (rate is None and period is None) or (rate is not None and period is not None):
+            raise Exception('Exactly one of rate or period may be used if both or none are specified an error will be '
+                            'raised')
+
+        if rate is not None:
+            self.rate = rate
+        if period is not None:
+            self.period = period
+        self._start_time = time.time()
+
+    @property
+    def rate(self):
+        return self._rate
+
+    @rate.setter
+    def rate(self, rate):
+        self._rate = rate
+        self._period = 1 / self._rate
+
+    @property
+    def period(self):
+        return self._period
+
+    @period.setter
+    def period(self, period):
+        self._period = period
+        self._rate = 1 / self._period
+
+    def __enter__(self):
+        self._start_time = time.time()
+
+    def __exit__(self, type, value, traceback):
+        elapsed = time.time() - self._start_time
+        if elapsed >= self._period:
+            # Enough time has already elapsed
+            return
+        # Sleep the remaining time
+        time.sleep(self._period - elapsed)
+
