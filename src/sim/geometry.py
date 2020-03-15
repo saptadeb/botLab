fece19d82e732a4da6462d617f797f164733b2b9
Kevin French <fnivek@gmail.com>
2020-03-15 10:01:42 -0400

    GEOMETRY: Create convinence class to convert between meters and pixels

--- src/sim/geometry.py
+++ src/sim/geometry.py
@@ -1,3 +1,6 @@
+import math
+
+
 class Pose:
     def __init__(self, x, y, theta):
         self.x = x
@@ -12,3 +15,22 @@ class Pose:
 
     def rotation(self):
         return self.theta
+
+
+class SpaceConverter:
+    def __init__(self, meters_per_pixel):
+        self._meters_per_pixel = meters_per_pixel
+
+    @property
+    def meters_per_pixel(self):
+        return self._meters_per_pixel
+
+    @meters_per_pixel.setter
+    def meters_per_pixel(self, meters_per_pixel):
+        self._meters_per_pixel = meters_per_pixel
+
+    def to_pixel(self, meters):
+        return math.ceil(meters / self._meters_per_pixel)
+
+    def to_meters(self, pixels):
+        return pixels * self._meters_per_pixel
