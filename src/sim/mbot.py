a1bba0c227d80712aa3453fcc4e5f27b3cc2e1cd
Kevin French <fnivek@gmail.com>
2020-03-18 08:25:48 -0400

    SIM: Move sim logic from mbot to sim

--- src/sim/mbot.py
+++ src/sim/mbot.py
@@ -3,69 +3,36 @@ import geometry
 import math
 import lcm
 import sys
+import time
 import threading
 from timing import Rate
 
 sys.path.append('../lcmtypes')
-from odometry_t import odometry_t
 from mbot_motor_command_t import mbot_motor_command_t
-from lidar_t import lidar_t
-from timestamp_t import timestamp_t
 
 
 class Mbot(pygame.sprite.Sprite):
     def __init__(self):
         super(Mbot, self).__init__()
+
+        # Model
         self._pose = geometry.Pose(4.5, 4.5, math.pi / 2)
+        stop = mbot_motor_command_t()
+        stop.utime = int(time.time() * 1e6)
+        self.current_motor_commands = [stop]
         self._radius = 0.1
+
+        # View
         self._primary_color = pygame.Color(0, 0, 255)
         self._secondary_color = pygame.Color(255, 0, 0)
         self._dir_indicator_arc_length = math.pi / 3
         self.image = pygame.Surface([10, 10])
         self.image.set_colorkey((0, 0, 0))
         self.rect = self.image.get_rect()
-        # LCM update rate
-        self._lcm_handle_rate = 50
-        # LCM channel names
-        self._odometry_channel = 'ODOMETRY'
-        self._lidar_channel = 'LIDAR'
-        self._motor_command_channel = 'MBOT_MOTOR_COMMAND'
-        self._timesync_channel = 'MBOT_TIMESYNC'
-        # Subscribe to lcm topics
-        self._lcm = lcm.LCM("udpm://239.255.76.67:7667?ttl=2")
-        self._lcm.subscribe(self._odometry_channel, self._odometry_handler)
-        self._lcm.subscribe(self._lidar_channel, self._lidar_handler)
-        self._lcm.subscribe(self._motor_command_channel, self._motor_command_handler)
-        self._lcm.subscribe(self._timesync_channel, self._timesync_handler)
-        # Start callback thread
-        self._lcm_thread = threading.Thread(target=self._handle_lcm)
-        self._lcm_thread.start()
 
-    def _handle_lcm(self):
-        try:
-            while True:
-                with Rate(self._lcm_handle_rate):
-                    self._lcm.handle_timeout(1000.0 / self._lcm_handle_rate)
-        except KeyboardInterrupt:
-            print("lcm exit!")
-            sys.exit()
+    """ View """
 
-    def _odometry_handler(self, channel, data):
-        msg = odometry_t.decode(data)
-
-    def _lidar_handler(self, channel, data):
-        msg = lidar_t.decode(data)
-
-    def _motor_command_handler(self, channel, data):
-        msg = mbot_motor_command_t.decode(data)
-        self._pose.theta += msg.angular_v
-        self._pose.x += msg.trans_v * math.cos(-self._pose.theta)
-        self._pose.y += msg.trans_v * math.sin(-self._pose.theta)
-
-    def _timesync_handler(self, channel, data):
-        msg = timestamp_t.decode(data)
-
-    def update(self, space_converter):
+    def _render(self, space_converter):
         radius = space_converter.to_pixel(self._radius)
         self.image = pygame.Surface([radius * 2.5, radius * 2.5])
         self.image.set_colorkey((0, 0, 0))
@@ -81,3 +48,8 @@ class Mbot(pygame.sprite.Sprite):
             self._pose.rotation() + self._dir_indicator_arc_length / 2,
             5
         )
+
+    """ Controller """
+
+    def update(self, space_converter):
+        self._render(space_converter)
--- src/sim/sim.py
+++ src/sim/sim.py
@@ -5,25 +5,50 @@ from timing import Rate
 from lidar import Lidar
 from geometry import SpaceConverter
 import math
+import time
+import threading
 from pygame.locals import *
 import sys
 import lcm
 sys.path.append('../lcmtypes')
+from odometry_t import odometry_t
 from mbot_motor_command_t import mbot_motor_command_t
+from lidar_t import lidar_t
+from timestamp_t import timestamp_t
 
 
 class Gui:
     def __init__(self):
+        # Model
+        self._map = None
+        self._mbot = Mbot()
+        self._lidar = Lidar()
+
+        # Controller
+        self._lcm = lcm.LCM("udpm://239.255.76.67:7667?ttl=2")
+        # LCM update rate
+        self._lcm_handle_rate = 50
+        # LCM channel names
+        self._odometry_channel = 'ODOMETRY'
+        self._lidar_channel = 'LIDAR'
+        self._motor_command_channel = 'MBOT_MOTOR_COMMAND'
+        self._timesync_channel = 'MBOT_TIMESYNC'
+        # Subscribe to lcm topics
+        self._lcm.subscribe(self._odometry_channel, self._odometry_handler)
+        self._lcm.subscribe(self._lidar_channel, self._lidar_handler)
+        self._lcm.subscribe(self._motor_command_channel, self._motor_command_handler)
+        self._lcm.subscribe(self._timesync_channel, self._timesync_handler)
+        # Start callback thread
+        self._lcm_thread = threading.Thread(target=self._handle_lcm)
+        self._lcm_thread.start()
+
+        # View
         self._running = True
         self._display_surf = None
-        self._map = None
         self._size = self._width, self._height = 640, 400 * 2
         self._sprites = pygame.sprite.RenderUpdates()
-        self._mbot = Mbot()
+        self._max_frame_rate = 50
         self._sprites.add(self._mbot)
-        self._frame_rate = 50
-        self._lcm = lcm.LCM("udpm://239.255.76.67:7667?ttl=2")
-        self._lidar = Lidar()
         self._space_converter = None
 
     def on_init(self):
@@ -41,6 +66,8 @@ class Gui:
         self._running = True
         self._lidar.start()
 
+    """ Controller """
+
     def on_event(self, event):
         if event.type == pygame.QUIT:
             self._running = False
@@ -49,7 +76,48 @@ class Gui:
         msg = mbot_motor_command_t()
         msg.trans_v = 0.01
         msg.angular_v = 0.1
-        self._lcm.publish('MBOT_MOTOR_COMMAND', msg.encode())
+        # self._lcm.publish('MBOT_MOTOR_COMMAND', msg.encode())
+
+    def on_execute(self):
+        if self.on_init() == False:
+            self._running = False
+
+        while( self._running ):
+            with Rate(self._max_frame_rate):
+                for event in pygame.event.get():
+                    self.on_event(event)
+                self.on_loop()
+                self.on_render()
+
+        self.on_cleanup()
+
+    """ Controller LCM """
+
+    def _handle_lcm(self):
+        try:
+            while True:
+                with Rate(self._lcm_handle_rate):
+                    self._lcm.handle_timeout(1000.0 / self._lcm_handle_rate)
+        except KeyboardInterrupt:
+            print("lcm exit!")
+            sys.exit()
+
+    def _odometry_handler(self, channel, data):
+        msg = odometry_t.decode(data)
+
+    def _lidar_handler(self, channel, data):
+        msg = lidar_t.decode(data)
+
+    def _motor_command_handler(self, channel, data):
+        msg = mbot_motor_command_t.decode(data)
+        # Override utime with sim time
+        msg.utime = int(time.time() * 1e6)
+        self.mbot.current_motor_commands.append(msg)
+
+    def _timesync_handler(self, channel, data):
+        msg = timestamp_t.decode(data)
+
+    """ View """
 
     def on_render(self):
         self._sprites.clear(self._display_surf, self._map.image)
@@ -60,18 +128,6 @@ class Gui:
     def on_cleanup(self):
         pygame.quit()
 
-    def on_execute(self):
-        if self.on_init() == False:
-            self._running = False
-
-        while( self._running ):
-            with Rate(self._frame_rate):
-                for event in pygame.event.get():
-                    self.on_event(event)
-                self.on_loop()
-                self.on_render()
-
-        self.on_cleanup()
 
 if __name__ == "__main__" :
     sim = Gui()
