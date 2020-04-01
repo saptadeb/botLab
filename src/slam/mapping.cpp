bf02900aec920007f8e14e7b75974527dae8babd
Saptadeep <saptadeb@umich.edu>
2020-03-31 21:54:39 -0400

    mapping function implemented

--- src/slam/mapping.cpp
+++ src/slam/mapping.cpp
@@ -9,6 +9,7 @@ Mapping::Mapping(float maxLaserDistance, int8_t hitOdds, int8_t missOdds)
 : kMaxLaserDistance_(maxLaserDistance)
 , kHitOdds_(hitOdds)
 , kMissOdds_(missOdds)
+, initialized_(false)
 {
 }
 
@@ -16,4 +17,119 @@ Mapping::Mapping(float maxLaserDistance, int8_t hitOdds, int8_t missOdds)
 void Mapping::updateMap(const lidar_t& scan, const pose_xyt_t& pose, OccupancyGrid& map)
 {
     //////////////// TODO: Implement your occupancy grid algorithm here ///////////////////////
+    if(!initialized_)
+    {
+        previousPose_ = pose;
+    }
+
+    MovingLaserScan movingScan(scan, previousPose_, pose);
+
+    for(auto& ray : movingScan)
+    {
+        scoreEndpoint(ray, map);
+    }
+
+
+    for(auto& ray: movingScan)
+    {
+      scoreRay(ray,map);
+    }
+
+
+    initialized_ = true;
+    previousPose_ = pose;
+
+}
+
+void Mapping::scoreEndpoint(const adjusted_ray_t& ray, OccupancyGrid& map){
+  if(ray.range <= kMaxLaserDistance_)
+  {
+    Point<float> rayStart = global_position_to_grid_position(ray.origin, map);
+    Point<int> rayCell;
+
+    rayCell.x = static_cast<int>((ray.range * std::cos(ray.theta) * map.cellsPerMeter()) + rayStart.x);
+    rayCell.y = static_cast<int>((ray.range * std::sin(ray.theta) * map.cellsPerMeter()) + rayStart.y);
+
+    // increaseCellOdds(rayCell.x,rayCell.y,map);
+
+    if(map.isCellInGrid(rayCell.x,rayCell.y)){
+      increaseCellOdds(rayCell.x,rayCell.y,map);
+    }
+  }
+}
+
+void Mapping::scoreRay(const adjusted_ray_t& ray, OccupancyGrid& map){
+  if(ray.range <= kMaxLaserDistance_)
+  {
+    Point<float> rayStart = global_position_to_grid_position(ray.origin, map);
+    Point<int> rayCell;
+
+    rayCell.x = static_cast<int>((ray.range * std::cos(ray.theta) * map.cellsPerMeter()) + rayStart.x);
+    rayCell.y = static_cast<int>((ray.range * std::sin(ray.theta) * map.cellsPerMeter()) + rayStart.y);
+
+    bresenham(rayStart.x,rayStart.y,rayCell.x,rayCell.y,map);
+
+    // if(map.isCellInGrid(rayCell.x,rayCell.y)){
+    //   bresenham(rayStart.x,rayStart.y,rayCell.x,rayCell.y,map);
+    // }
+  }
+}
+
+void Mapping::increaseCellOdds(int x, int y, OccupancyGrid& map){
+  if(!initialized_){
+    //do nothing
+  }
+
+  else if(std::numeric_limits<CellOdds>::max() - map(x,y) > kHitOdds_){
+    map(x,y) += kHitOdds_;
+  }
+
+  else{
+    map(x,y) = std::numeric_limits<CellOdds>::max();
+  }
+}
+
+void Mapping::bresenham(int x1,int y1,int x2,int y2,OccupancyGrid& map)
+{
+	int dx,dy,sx,sy,err,x,y;
+  float e2;
+	dx=std::abs(x2-x1);
+	dy=std::abs(y2-y1);
+  sx = x1<x2 ? 1 : -1;
+  sy = y1<y2 ? 1 : -1;
+	err = (dx) - (dy);
+  x = x1;
+  y = y1;
+
+	while(x != x2 || y != y2)
+	{
+    if(map.isCellInGrid(x,y)){
+      decreaseCellOdds(x,y,map);
+    }
+    e2 = 2*err;
+    if(e2 >= -dy)
+		{
+			err -= dy;
+      x += sx;
+		}
+		if(e2 <= dx)
+		{
+			err += dx;
+      y += sy;
+		}
+	}
+}
+
+void Mapping::decreaseCellOdds(int x, int y, OccupancyGrid& map){
+  if(!initialized_){
+    //do nothing
+  }
+
+  else if(map(x,y) - kMissOdds_ > std::numeric_limits<CellOdds>::min()){
+    map(x,y) -= kMissOdds_;
+  }
+
+  else{
+    map(x,y) = std::numeric_limits<CellOdds>::min();
+  }
 }
--- src/slam/mapping.hpp
+++ src/slam/mapping.hpp
@@ -6,26 +6,27 @@
 
 class OccupancyGrid;
 class lidar_t;
+struct adjusted_ray_t;
 
 /**
-* Mapping implements the occupancy grid mapping algorithm. 
+* Mapping implements the occupancy grid mapping algorithm.
 */
 class Mapping
 {
 public:
-    
+
     /**
     * Constructor for Mapping.
-    * 
+    *
     * \param    maxLaserDistance    Maximum distance for the rays to be traced
     * \param    hitOdds             Increase in occupied odds for cells hit by a laser ray
     * \param    missOdds            Decrease in occupied odds for cells passed through by a laser ray
     */
     Mapping(float maxLaserDistance, int8_t hitOdds, int8_t missOdds);
-    
+
     /**                                                                                     *
     * updateMap incorporates information from a new laser scan into an existing OccupancyGrid.
-    * 
+    *
     * \param    scan            Laser scan to use for updating the occupancy grid
     * \param    pose            Pose of the robot at the time when the last ray was measured
     * \param    map             OccupancyGrid instance to be updated
@@ -33,12 +34,22 @@ public:
     void updateMap(const lidar_t& scan, const pose_xyt_t& pose, OccupancyGrid& map);
 
 private:
-    
+
     const float  kMaxLaserDistance_;
     const int8_t kHitOdds_;
     const int8_t kMissOdds_;
-    
+
     //////////////////// TODO: Add any private members needed for your occupancy grid mapping algorithm ///////////////
+
+pose_xyt_t previousPose_;
+bool initialized_;
+
+
+void scoreEndpoint(const adjusted_ray_t& ray, OccupancyGrid& map);
+void scoreRay(const adjusted_ray_t& ray, OccupancyGrid& map);
+void increaseCellOdds(int x, int y, OccupancyGrid& map);
+void bresenham(int x1, int y1, int x2, int y2, OccupancyGrid& map);
+void decreaseCellOdds(int x, int y, OccupancyGrid& map);
 };
 
 #endif // SLAM_MAPPING_HPP
